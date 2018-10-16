b9c78ebda7744359166811bf58ad34565a13d1c7
Ben Patterson <bmizepatterson@gmail.com>
2018-10-16 10:13:13 -0400

    WIP: Display start sequence

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-
+let startScreen = null;
+let gameStarted = false;
+let snake = {
+    direction: 'up',
+    length: 1
+}
 
 function setup() {
     let canvas = createCanvas(500, 500);
     canvas.parent('app');
+    startScreen = document.getElementById('start-button');
+    startScreen.querySelector('button').onclick = start;
 }
 
 function draw() {
+    if (gameStarted) {
 
+    }
+
+}
+
+function drawGrid() {
+}
+
+function start() {
+    startScreen.classList.add('wink-out');
+    // start sequence
+    let timer = 3;
+    fill(255);
+    setTimeout(function() {
+        text(timer, 10, 30);
+    }, 1000)
 }
